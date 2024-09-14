b17a42c6ee04977e0882a083ebe4c836d2c626f8
Ava Pek <code@exodrifter.space>
2024-09-14 18:56:21 -0500

    Add flake for building website

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A flake for building exodrifter's website";
+  inputs = {
+    utils.url = "github:numtide/flake-utils/v1.0.0";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem(
+      system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              pkgs.nodejs_22
+            ];
+          };
+          packages = rec {
+            default = pkgs.buildNpmPackage rec {
+              pname = "quartz";
+              version = "4.3.1";
+
+              src = pkgs.fetchFromGitHub {
+                owner = "jackyzha0";
+                repo = "quartz";
+                rev = "v${version}";
+                hash = "sha256-kID0R/n3ij5uvZ/CXjiLa3oqjghX2U4Zu82huejG6/Q=";
+              };
+
+              dontNpmBuild = true;
+              makeCacheWritable = true;
+
+              npmDepsHash = "sha256-qgAzMTtFTShj3xUut73DBCbkt7yTwVjthL8hEgRFdIo=";
+
+              installPhase = ''
+                runHook preInstall
+                npmInstallHook
+
+                cd $out/lib/node_modules/@jackyzha0/quartz
+                rm -r ./content
+                mkdir content
+                cp -r ${./.}/* ./content
+                $out/bin/quartz build
+                mv public/ $out/public/
+
+                runHook postInstall
+              '';
+            };
+          };
+        }
+    );
+}
