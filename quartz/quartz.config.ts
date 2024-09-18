d7559c66d6d78cd6e1079a202e33338165a100f4
Ava Pek <code@exodrifter.space>
2024-09-18 04:19:15 -0500

    Add some rizz

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -27,7 +27,7 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
           light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          lightgray: "#c5c5c5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
--- quartz/quartz/components/BrandingLogo.tsx
+++ quartz/quartz/components/BrandingLogo.tsx
@@ -10,11 +10,11 @@ export default ((options: BrandingOptions) => {
   function BrandingLogo({ fileData, displayClass }: QuartzComponentProps) {
     const baseDir = pathToRoot(fileData.slug!)
     return (
-      <h2 class={classNames(displayClass, "page-title")}>
+      <div class={classNames(displayClass, "page-title")}>
         <a href={baseDir}>
           <img src={joinSegments(baseDir, options.path)} alt="exodrifter logo" draggable="false" />
         </a>
-      </h2>
+      </div>
     )
   }
 
