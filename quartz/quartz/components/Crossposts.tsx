4e053cfc84bde5d4859456a27dab1d5c4a09970f
Ava Pek <code@exodrifter.space>
2024-09-19 01:44:09 -0500

    Add icons to crossposts component

--- quartz/quartz/components/Crossposts.tsx
+++ quartz/quartz/components/Crossposts.tsx
@@ -18,19 +18,75 @@ const Crossposts: QuartzComponent = ({
     return b.time - a.time;
   })
   return (
-    <div class={classNames(displayClass, "backlinks")}>
+    <div class={classNames(displayClass, "crossposts")}>
       <h3>Crossposts</h3>
       <ul class="overflow">
-        {crossposts.map((f) => (
-            <li>
-              {formatDate(new Date(f.time)!, cfg.locale)}
-              <br/>
-              <a href={f.url} class="internal">
-                {new URL(f.url).host}
-              </a>
-            </li>
-          ))
-        }
+        {crossposts.map((f) => {
+          const host = new URL(f.url).host
+          switch (host) {
+
+            case "cohost.org":
+            case "www.cohost.org":
+              return (
+                <li>
+                  {formatDate(new Date(f.time)!, cfg.locale)}
+                  <br/>
+                  <img src="/static/cohost.svg"/>&nbsp;
+                  <a href={f.url} class="external">
+                    cohost!
+                  </a>
+                </li>
+              )
+
+            case "exodrifter.itch.io":
+              return (
+                <li>
+                  {formatDate(new Date(f.time)!, cfg.locale)}
+                  <br/>
+                  <img src="/static/itch.svg"/>&nbsp;
+                  <a href={f.url} class="external">
+                    itch.io
+                  </a>
+                </li>
+              )
+
+            case "patreon.com":
+            case "www.patreon.com":
+              return (
+                <li>
+                  {formatDate(new Date(f.time)!, cfg.locale)}
+                  <br/>
+                  <i class="ri-patreon-fill"></i>&nbsp;
+                  <a href={f.url} class="external">
+                    Patreon
+                  </a>
+                </li>
+              )
+
+            case "store.steampowered.com":
+              return (
+                <li>
+                  {formatDate(new Date(f.time)!, cfg.locale)}
+                  <br/>
+                  <i class="ri-steam-fill"></i>&nbsp;
+                  <a href={f.url} class="external">
+                    Steam
+                  </a>
+                </li>
+              )
+
+            default:
+              return (
+                <li>
+                  {formatDate(new Date(f.time)!, cfg.locale)}
+                  <br/>
+                  <a href={f.url} class="external">
+                    {host}
+                  </a>
+                </li>
+              )
+          }
+        })}
       </ul>
     </div>
   )
