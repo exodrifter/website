74b40dbb96cf69cb06736fb9a79987a7a9810783
Ava Pek <code@exodrifter.space>
2025-07-14 12:50:57 -0700

    Align Vimeo and YouTube embeds to the grid

--- src/Exo/Pandoc.hs
+++ src/Exo/Pandoc.hs
@@ -155,11 +155,10 @@ convertVideoEmbeds =
 
       _ -> inline
 
--- Same as the official YouTube embed code
+-- Same as the official YouTube embed code, with the styling removed.
 makeYoutubeEmbed :: Text -> Text
 makeYoutubeEmbed v =
-  "<iframe width=\"560\" height=\"315\" \
-    \src=\"https://www.youtube.com/embed/" <> v <> "\" \
+  "<iframe src=\"https://www.youtube.com/embed/" <> v <> "\" \
     \title=\"YouTube video player\" \
     \frameborder=\"0\" \
     \allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" \
@@ -167,19 +166,16 @@ makeYoutubeEmbed v =
     \allowfullscreen>\
   \</iframe>"
 
--- Almost the same as the official Vimeo embed code; the only difference is that
--- when you generate Vimeo embeds online, it uses the name of the video as the
--- title attribute for the iframe.
+-- Almost the same as the official Vimeo embed code with the styling removed;
+-- the only difference is that when you generate Vimeo embeds online, it uses
+-- the name of the video as the title attribute for the iframe.
 makeVimeoEmbed :: Text -> Text
 makeVimeoEmbed v =
-  "<div style=\"padding:56.25% 0 0 0;position:relative;\">\
-  \<iframe src=\"https://player.vimeo.com/video/" <> v <> "?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\" \
+  "<iframe src=\"https://player.vimeo.com/video/" <> v <> "?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\" \
     \frameborder=\"0\" \
     \allow=\"autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share\" \
-    \style=\"position:absolute;top:0;left:0;width:100%;height:100%;\" \
     \title=\"Vimeo video player\">\
   \</iframe>\
-  \</div>\
   \<script src=\"https://player.vimeo.com/api/player.js\">\
   \</script>"
 
